bc900b73abdd6371651d23baa9a73e5e40c4183a
visualstudio <visualstudio@230-PC03>
2019-02-28 10:33:09 -0500

    1

--- 735952Feb28Week08/js/main.js
+++ 735952Feb28Week08/js/main.js
@@ -1,1 +1,14 @@
 ﻿// Your code here!
+const circle = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    }
+    ,
+    draw: function () {
+        console.log('draw')
+            ;
+    }
+};
+circle.draw();
